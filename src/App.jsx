2d5a21446a75675aa0a5cad4efecac8e487748b6
idahmed <idahmedyassine@gmail.com>
2026-03-29 11:32:00 +0100

    add analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import PortfolioPage from './pages/PortfolioPage'
 import { SpeedInsights } from "@vercel/speed-insights/react"
+import { Analytics } from '@vercel/analytics/react';
+
 
 function App() {
   return (
     <>
       <PortfolioPage />
       <SpeedInsights />
+      <Analytics />
     </>
   )
 }
